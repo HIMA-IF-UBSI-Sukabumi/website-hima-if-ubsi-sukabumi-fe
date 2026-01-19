3e43e56924a78b0d69d17e79317dc7da99130b12
NaraMizaru <hilal.muhammad0807@gmail.com>
2026-01-19 09:32:08 +0700

    add metadata for landing page with title and description

--- src/app/(portal)/page.tsx
+++ src/app/(portal)/page.tsx
@@ -1,4 +1,10 @@
+import type {Metadata} from 'next';
 import ModulePortalLandingPage from "@/modules/portal/pages/landing.page";
+
+export const metadata: Metadata = {
+    title: "Beranda - HIMA-IF UBSI PSDKU Sukabumi",
+    description: "Website Resmi Himpunan Mahasiswa Informatika Universitas Bina Sarana Informatika PSDKU Sukabumi",
+}
 
 const Landing = () => {
     return (
